53689e6551d77e410f914d0ff9fc11869c0afa80
huhaonan <huhaonan.en@gmail.com>
2016-10-02 17:23:44 +0800

    minor fix

--- src/main/java/com/dajia/DajiaAdmin.java
+++ src/main/java/com/dajia/DajiaAdmin.java
@@ -2,10 +2,8 @@ package com.dajia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
 @EnableAutoConfiguration
